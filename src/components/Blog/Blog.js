640e69745f888d682018b6107bb68fc863f17bd1
arupmondol <arupmondol399@gmail.com>
2022-10-27 23:55:53 +0600

    all

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -8,7 +8,7 @@ const Blog = () => {
   return (
 <div className='blog'>
 <h2> What is cors?</h2> 
-<p>Cross-origin resource haring (CORS) is a browser mechanism which enables controlled access to resources located outside of a given domain. It extends and adds flexibility to the same-origin policy (SOP). However, it also provides potential for cross-domain attacks, if a website's CORS policy is poorly configured and implemented. CORS is not a protection against cross-origin attacks such as cross-site request forgery (CSRF).</p>
+<p>Cross-origin resource sharing (CORS) is a browser mechanism which enables controlled access to resources located outside of a given domain. It extends and adds flexibility to the same-origin policy (SOP). However, it also provides potential for cross-domain attacks, if a website's CORS policy is poorly configured and implemented. CORS is not a protection against cross-origin attacks such as cross-site request forgery (CSRF).</p>
 
 <h2>Why are using firebase?</h2>
 <p>What other Options do you have ti implement authentication
